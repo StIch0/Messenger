bd0f3c07c1fb7fd5cfa380c71382bce11f913bf8
StIch0 <you@example.com>
2018-05-11 12:01:29 +0800

    Button and image closure

--- Messenger/ViewControllers/StartViewController.swift
+++ Messenger/ViewControllers/StartViewController.swift
@@ -9,56 +9,55 @@
 import Foundation
 import UIKit
 class StartViewController : UIViewController {
-    var image : UIImageView!
-    var inputButton : UIButton!
+    var startImage : UIImageView = {
+        var image = UIImageView(image: #imageLiteral(resourceName: "StartScreen"))
+        image.layer.shadowOffset = CGSize(width: 0.2, height: 0.2)
+        image.layer.shadowColor = UIColor(rgb: 0x000000, alfa: 0.5).cgColor
+        image.layer.shadowRadius = 4
+        return image
+    }()
+    
+    var inputButton : UIButton = {
+        var button = UIButton()
+        button.setTitle("Войти", for: .normal)
+        button.backgroundColor = UIColor(rgb: 0xCF1F28, alfa : 1)
+        button.layer.shadowColor = UIColor(rgb: 0xE4222D, alfa: 0.5).cgColor
+        button.layer.shadowRadius = 9
+        button.layer.shadowOffset = CGSize(width: 0.2, height: 0.2)
+        button.setTitleShadowColor(UIColor.init(rgb: 0xFFFFFF, alfa: 1), for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.titleLabel?.textColor = UIColor(rgb: 0xFFFFFF, alfa: 1)
+        print(button.frame.height)
+        print(button.bounds.height)
+        print(button.layer.frame.height)
+        print(button.layer.bounds.height)
+        button.layer.cornerRadius = button.bounds.height
+        return button
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(rgb: 0xFFFFFF, alfa: 1)
         setUpImageView()
-        setUpAnchorViewButton()
+        seUpButtonView()
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-        seUpButtonView()
     }
     func setUpImageView (){
-        image = UIImageView(image: #imageLiteral(resourceName: "StartScreen"))
-        view.addSubview(image)
-        image.backgroundColor = .white
-        image.translatesAutoresizingMaskIntoConstraints = false
-        image.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        image.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        image.layer.shadowOffset = CGSize(width: 0.2, height: 0.2)
-        image.layer.shadowColor = UIColor(rgb: 0x000000, alfa: 0.5).cgColor
-        image.layer.shadowRadius = 4
+        view.addSubview(startImage)
+        startImage.backgroundColor = .white
+        startImage.translatesAutoresizingMaskIntoConstraints = false
+        startImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        startImage.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
-    func setUpAnchorViewButton(){
-        inputButton = UIButton(type: .custom)
+
+    func seUpButtonView(){
         view.addSubview(inputButton)
         inputButton.translatesAutoresizingMaskIntoConstraints = false
         inputButton.bottomAnchor.constraint(equalTo: view.bottomAnchor)         .isActive = true
-        if #available(iOS 11.0, *) {
-            inputButton.trailingAnchor.constraint(equalTo: image.trailingAnchor).isActive = true
-        } else {
-            // Fallback on earlier versions
-        }
-        if #available(iOS 11.0, *) {
-            inputButton.leadingAnchor.constraint(equalTo: image.leadingAnchor)  .isActive = true
-        } else {
-            // Fallback on earlier versions
-        }
-        inputButton.heightAnchor.constraint(equalTo: image.heightAnchor)        .isActive = true
-     }
-    func seUpButtonView(){
-        inputButton.clipsToBounds = true
-        inputButton.setTitle("Войти", for: .normal)
-        inputButton.backgroundColor = UIColor(rgb: 0xCF1F28, alfa : 1)
-        inputButton.layer.shadowColor = UIColor(rgb: 0xE4222D, alfa: 0.5).cgColor
-        inputButton.layer.shadowRadius = 9
-        inputButton.layer.shadowOffset = CGSize(width: 0.2, height: 0.2)
-        inputButton.setTitleShadowColor(UIColor.init(rgb: 0xFFFFFF, alfa: 1), for: .normal)
-        inputButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
-        inputButton.layer.cornerRadius = inputButton.layer.frame.height
+        inputButton.trailingAnchor.constraint(equalTo: startImage.trailingAnchor)    .isActive = true
+        inputButton.leadingAnchor.constraint(equalTo: startImage.leadingAnchor)      .isActive = true
+        inputButton.heightAnchor.constraint(equalTo: startImage.heightAnchor)        .isActive = true
         inputButton.addTarget(self, action: #selector(loadMainView), for: .touchDown)
     }
     
